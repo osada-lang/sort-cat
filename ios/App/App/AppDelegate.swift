b02e0105410431fa716f978926dcf129af7268cb
Kent Osada <kentosada@KentnoMacBook-Air.local>
2026-04-22 13:52:15 +0900

    Build 9: Re-add native ATT request and maintain splash screen fix

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Capacitor
+import AppTrackingTransparency
+import AdSupport
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -27,6 +29,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { status in
+                // ATTリクエスト完了後の処理
+            }
+        }
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
